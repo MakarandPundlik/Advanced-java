d7b75808bfd7b2dea56c6c20e7dac2bb0db04be6
MakarandPundlik <mmp2308@gmail.com>
2021-01-04 16:01:22 +0530

    program on callablestatment

---
+++ JDBC/Prog12.java
@@ -0,0 +1,71 @@
+//write a program to call mysql procedure "changeadata()" with 2 ip and 1 op parameters
+//accept customerid and full name from cmd
+import java.sql.*;
+
+class Prog12
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			int cid = Integer.parseInt(args[0]);
+			String fn = args[1];
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			String url = "jdbc:mysql://localhost:3306/company_info";
+			String username = "root";
+			String password = "1234";
+			
+			Connection conn = DriverManager.getConnection(url,username,password);
+			
+			CallableStatement cstmt = conn.prepareCall("changeadata(?,?,?)");
+			
+			//set values for parameters 
+			cstmt.setInt(1,cid);
+			cstmt.setString(2,fn);
+			
+			/*while working with mysql procedures we may have to execute different types*
+			of queries like insert,updates,delete,select etc.
+			All these queries can be executed by calling the execute method of callable statement object
+			*/
+			
+			cstmt.execute();
+			
+			//now get the value of parameters for validation
+			
+			int nor = cstmt.getInt(3); //nor will contain the records affected by query 
+			
+			if(nor !=0)
+			System.out.println("record with custid "+cid+" has been updated");
+			
+			else
+			System.out.println("record with custid "+cid+" not found");
+			
+			ResultSet rs = cstmt.getResultSet();
+			
+			//display the column headings first
+			
+			System.out.println("Cid\tFullname");
+			
+			while(rs.next())
+			System.out.println(rs.getInt(1)+"\t"+rs.getString(2));
+			
+			rs.close();
+			cstmt.close();
+			conn.close();
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("Please supply customer id and full name");
+			
+		}
+		catch(NumberFormatException e)
+		{
+			System.out.println("Please supply customer id in integer format only");
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+	}
+}
